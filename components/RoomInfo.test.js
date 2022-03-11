82bec583ee1ac9516699aa5d91727cbdc0176116
MarcoGoC <marcogoc951@gmail.com>
2022-03-11 10:25:19 -0500

    roominfo box

---
+++ components/RoomInfo.test.js
@@ -0,0 +1,40 @@
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import { axe, toHaveNoViolations } from 'jest-axe'
+import RoomInfo from './RoomInfo'
+
+expect.extend(toHaveNoViolations)
+
+const showRoomInfo = {
+  id: 'room',
+  roomId: '7#&76',
+  playerName: 'some name',
+  playersOnline: 99,
+}
+
+describe('RoomInfo', () => {
+  it('renders RoomInfo', () => {
+    const primary = render(
+      <RoomInfo
+        id={showRoomInfo.id}
+        roomId={showRoomInfo.roomId}
+        playerName={showRoomInfo.playerName}
+        playerOnline={showRoomInfo.playersOnline}
+      />
+    )
+    expect(primary).toBeTruthy()
+  })
+
+  it('has no a11y violations', async () => {
+    const { container } = render(
+      <RoomInfo
+        id={showRoomInfo.id}
+        roomId={showRoomInfo.roomId}
+        playerName={showRoomInfo.playerName}
+        playerOnline={showRoomInfo.playersOnline}
+      />
+    )
+    const results = await axe(container)
+    expect(results).toHaveNoViolations()
+  })
+})
