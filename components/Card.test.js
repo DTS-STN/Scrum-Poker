21bce2aadab444048e781ba528b6d717a1143946
shewood <shewood01@gmail.com>
2022-03-17 08:17:02 -0400

    fixed act warning

--- components/Card.test.js
+++ components/Card.test.js
@@ -22,16 +22,18 @@ describe('Card', () => {
   })
 
   it('has no a11y violations', async () => {
-    const { container } = render(
-      <Card
-        src={fakeCard.src}
-        id={fakeCard.id}
-        alt={fakeCard.alt}
-        selected
-        onClick={() => {}}
-      />
-    )
-    const results = await axe(container)
-    expect(results).toHaveNoViolations()
+    await act(async () => {
+      const { container } = render(
+        <Card
+          src={fakeCard.src}
+          id={fakeCard.id}
+          alt={fakeCard.alt}
+          selected
+          onClick={() => {}}
+        />
+      )
+      const results = await axe(container)
+      expect(results).toHaveNoViolations()
+    })
   })
 })
