ec54cc98ad4dd7ca2d88a017dd710c0a6d30b73d
Thomas Dacey <daceytom2@gmail.com>
2022-03-23 08:49:43 -0400

    when running dev allow local graphlq

--- graphql/client.js
+++ graphql/client.js
@@ -1,11 +1,44 @@
 import fetch from 'cross-fetch'
-import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
+import { ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { createClient } from 'graphql-ws'
+
+//Checks if the code is being executed by the client or the server
+//The server can't handle subscriptions nor should it
+let isClient = typeof window !== 'undefined'
+
+const httpLink = new HttpLink({
+  uri: process.env.NEXT_CONTENT_GRAPHQL,
+  fetch,
+})
+
+let link = isClient
+  ? (() => {
+      const wsClient = createClient({
+        url: 'ws://localhost:4000/graphql',
+      })
+
+      const wsLink = new GraphQLWsLink(wsClient)
+
+      return split(
+        ({ query }) => {
+          const definition = getMainDefinition(query)
+          return (
+            definition.kind === 'OperationDefinition' &&
+            definition.operation === 'subscription'
+          )
+        },
+        wsLink,
+        httpLink
+      )
+    })()
+  : () => {
+      return httpLink
+    }
 
 const client = new ApolloClient({
-  link: new HttpLink({
-    uri: process.env.NEXT_CONTENT_GRAPHQL,
-    fetch,
-  }),
+  link: link,
   cache: new InMemoryCache(),
 })
 
