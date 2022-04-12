943fd8308a3c949b40c285ad63449276221a72f9
P0NDER0SA <mikepond11@gmail.com>
2022-04-12 12:40:12 -0400

    pulling in Kris' Changes

--- components/CreateRoom.js
+++ components/CreateRoom.js
@@ -9,6 +9,7 @@ import Container from '../components/Container'
 import TextInput from '../components/TextInput'
 import FormButton from './FormButton'
 import { ErrorLabel } from '../components/ErrorLabel'
+import Card from '../components/Card'
 
 import { useMutation } from '@apollo/client'
 import ADD_ROOM from '../graphql/mutations/addRoom.graphql'
@@ -16,12 +17,18 @@ import ADD_USER from '../graphql/mutations/addUser.graphql'
 import UPDATE_USER from '../graphql/mutations/updateUser.graphql'
 
 import { useRouter } from 'next/router'
+import { useState, useRef, useReducer } from 'react'
 import Cookies from 'js-cookie'
-import { useState } from 'react'
+import { cards } from '../utils/cards'
 
 export default function CreateRoom(props) {
   /* istanbul ignore next */
   const t = props.locale === 'en' ? en : fr
+
+  //useRef doesn't notify the browser when mutating the current ref, so forceUpdate allows us to trigger a re-render instead of using useState and having a delay
+  const [, forceUpdate] = useReducer((x) => x + 1, 0)
+  const cardList = useRef([0, 1, 2, 3, 5, 8, 13, 20, 1001, 1002])
+
   const router = useRouter()
 
   //Load GraphQL Data
@@ -46,6 +53,19 @@ export default function CreateRoom(props) {
   //error handling stuff
   const [msg, setMsg] = useState('')
   const [hasError, setHasError] = useState(false)
+
+  //Card component handler
+  const onCardClickHandler = async (e, card) => {
+    if (cardList.current.includes(card.value)) {
+      let filteredList = cardList.current.filter(
+        (cardValue) => cardValue !== card.value
+      )
+      cardList.current = filteredList
+    } else {
+      cardList.current.push(card.value)
+    }
+    forceUpdate()
+  }
 
   //************************************* */
   //SUBMIT FORM BUSINESS LOGIC
@@ -134,6 +154,37 @@ export default function CreateRoom(props) {
           required={t.required}
           errors={errors.owner}
         />
+        <div className="mt-6 w-full">
+          <div className="block rounded-t-lg border-t border-l border-r border-gray-300 px-3 py-2 bg-gray-300">
+            {t.selectCards}{' '}
+            <span className="text-red-800 font-body" aria-hidden="true">
+              {t.required}
+            </span>
+          </div>
+          <ul className="pt-2 grid grid-cols-4 lg:grid-cols-5 xl:grid-cols-8 rounded-b-lg border-gray-300 border-b border-x">
+            {cards.map((card) => {
+              return (
+                <li className="px-1 pb-1" key={card.id}>
+                  <label htmlFor={card.id}>
+                    <Card
+                      src={card.src}
+                      id={card.id}
+                      alt={card.alt}
+                      onClick={(e) => onCardClickHandler(e, card)}
+                      onKeyDown={(e) => {
+                        if (e.keyCode === 32 || e.keyCode === 13) {
+                          onCardClickHandler(e, card)
+                        }
+                      }}
+                      homePage
+                      selected={cardList.current.includes(card.value)}
+                    />
+                  </label>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
         <div className="justify-center flex-auto">
           <FormButton text={t.createRoomButton} />
         </div>
