497ea20c38ab1ca971285593f18e48226dfd5177
Alex Yan <alexunique0519@gmail.com>
2022-04-13 11:04:23 -0400

    Update CountdownTimer.test.js

--- components/CountdownTimer.test.js
+++ components/CountdownTimer.test.js
@@ -4,8 +4,14 @@ import CountdownTimer from './CountdownTimer'
 
 describe('CountdownTimer test', () => {
   it('should render a countdown timer', () => {
+    jest.useFakeTimers()
     render(<CountdownTimer duration={10000} timeIsUpText={'time is up'} />)
     const timerText = screen.getByText('00:10')
     expect(timerText).toBeInTheDocument()
+    setTimeout(() => {
+      const timeIsUp = screen.getByText('time is up')
+      expect(timeIsUp).toBeInTheDocument()
+    }, 10001)
+    jest.runAllTimers()
   })
 })
